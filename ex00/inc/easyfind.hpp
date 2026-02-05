62bc4ab457f2c7d0fcd57f08ee0843c39260484d
Antoine <antoinebouclier1@gmail.com>
2026-02-05 10:07:05 +0100

    Ex00: Done. need more test

--- ex00/inc/easyfind.hpp
+++ ex00/inc/easyfind.hpp
@@ -1,6 +1,27 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <iostream>
+#include <algorithm>
 
+class NoOccurenceFoundException : public std::exception
+{
+	public:
+		virtual const char *what() const throw()
+		{
+			return ("No occurence found.");
+		}
+};
+
+template <typename T>
+typename T::const_iterator	easyfind(const T &container, int value)
+{
+	typename T::const_iterator	it;
+
+	it = find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw	NoOccurenceFoundException();
+	return (it);
+}
 
 #endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,6 +1,20 @@
 #include "easyfind.hpp"
+#include <vector>
 
 int	main(void)
 {
+	std::vector<int>	values;
+
+	for (int i = 0; i < 10; i++)
+		values.push_back(i);
+	try
+	{
+		std::cout << *easyfind(values, 5) << std::endl;
+		std::cout << *easyfind(values, 20) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	
 }
