7ce76498ac10ccbd62d3d4d20a001a711df42aa9
Antoine <antoinebouclier1@gmail.com>
2026-02-05 12:16:01 +0100

    Ex01: Init Span.cpp

--- ex01/src/Span.cpp
+++ ex01/src/Span.cpp
@@ -1,2 +1,39 @@
 #include "Span.hpp"
 
+/* -- Constructors -- */
+Span::Span()
+{
+}
+Span::Span(unsigned int N)
+{
+
+}
+
+Span::Span(const Span &copy)
+{
+
+}
+
+Span	&Span::operator=(const Span &copy)
+{
+}
+
+Span::~Span()
+{
+}
+
+/* -- Member function -- */
+void			Span::addNumber(unsigned int number)
+{
+
+}
+
+unsigned int	Span::shortestSpan()
+{
+
+}
+
+unsigned int	Span::longestSpan()
+{
+
+}
