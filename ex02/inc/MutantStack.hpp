757b3892e1c817eedd85588559f8ab7f3e8630cf
Antoine <antoinebouclier1@gmail.com>
2026-02-10 13:14:50 +0100

    Ex02:

--- ex02/inc/MutantStack.hpp
+++ ex02/inc/MutantStack.hpp
@@ -1,7 +1,30 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
+#include <stack>
+#include <deque>
 
+template <typename T, class container=std::deque<T> >
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack(){}
+		~MutantStack(){}
+		MutantStack(const MutantStack &stack)
+		{
+			*this = stack;
+		}
+		typedef typename container::iterator	iterator;
+		iterator	begin()
+		{
+			return (this->c.begin());
+		}
+		iterator	end()
+		{
+			return (this->c.end());
+		}
+};
 
 #include "MutantStack.tpp"
 
