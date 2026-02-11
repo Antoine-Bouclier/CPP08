8643752d39daa37dd9e5a5a9beced0f16c7bc6ca
Antoine <antoinebouclier1@gmail.com>
2026-02-11 09:35:01 +0100

    Ex02: Done with enough test.

--- ex02/inc/MutantStack.hpp
+++ ex02/inc/MutantStack.hpp
@@ -1,29 +1,33 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <iostream>
 #include <stack>
 #include <deque>
 
-template <typename T, class container=std::deque<T> >
+template <typename T, class container = std::deque<T> >
 class MutantStack : public std::stack<T>
 {
 	public:
 		MutantStack(){}
 		~MutantStack(){}
-		MutantStack(const MutantStack &stack)
+		MutantStack(const MutantStack &stack) : std::stack<T, container>(stack) {}
+		MutantStack	&operator=(const MutantStack &stack)
 		{
-			*this = stack;
+			if (this != &stack)
+				std::stack<T, container>::operator=(stack);
+			return (*this);
 		}
+
 		typedef typename container::iterator	iterator;
-		iterator	begin()
-		{
-			return (this->c.begin());
-		}
-		iterator	end()
-		{
-			return (this->c.end());
-		}
+		typedef typename container::const_iterator	const_iterator;
+		
+		/* -- iterator -- */
+		iterator	begin() { return (this->c.begin()); }
+		iterator	end() { return (this->c.end()); }
+
+		/* -- const_iterator -- */
+		const_iterator	begin() const { return (this->c.begin()); }
+		const_iterator	end() const { return (this->c.end()); }
 };
 
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,59 +1,100 @@
 #include "MutantStack.hpp"
+#include <iostream>
 #include <list>
 
 int	main(void)
 {
 	/* -- TEST with MutantStack -- */
 	{
-		std::cout << "TEST with MutantStack" << std::endl;
+		std::cout << "TEST MutantStack" << std::endl;
 		MutantStack<int> mstack;
 		mstack.push(5);
 		mstack.push(17);
-		std::cout << mstack.top() << std::endl;
+		std::cout << "value on top: " << mstack.top() << std::endl;
 		mstack.pop();
-		std::cout << mstack.size() << std::endl;
+		std::cout << "size of the stack: " << mstack.size() << std::endl;
 		mstack.push(3);
 		mstack.push(5);
 		mstack.push(737);
-		//[...]
 		mstack.push(0);
+		std::cout << std::endl;
+
+		/* -- iterator -- */
 		MutantStack<int>::iterator it = mstack.begin();
 		MutantStack<int>::iterator ite = mstack.end();
 		++it;
 		--it;
+		std::cout << "iterator: ";
 		while (it != ite)
 		{
-		std::cout << *it << std::endl;
-		++it;
+			std::cout << *it << " ";
+			++it;
 		}
-		std::stack<int> s(mstack);
+		std::cout << std::endl;
+
+		/* -- const iterator -- */
+		MutantStack<int>::const_iterator	cit = mstack.begin();
+		MutantStack<int>::const_iterator	cite = mstack.end();
+		std::cout << "const_iterator: ";
+		while (cit != cite)
+		{
+			std::cout << *cit << " ";
+			++cit;
+		}
+		std::cout << std::endl;
+
+		/* -- Test copy -- */
+		MutantStack<int>	copy(mstack);
+		std::cout << "\ntest copy: " << std::endl;
+		copy.pop();
+		std::cout << "size of mstack: " << mstack.size() << std::endl;
+		std::cout << "size of copy: " << copy.size() << std::endl;
+		std::cout << std::endl;
+
+		copy = mstack;
+		std::cout << "size of mstack: " << mstack.size() << std::endl;
+		std::cout << "size of copy: " << copy.size() << std::endl;
 		std::cout << std::endl;
 	}
 
-	/* -- TEST with MutantStack -- */
+	/* -- TEST with list -- */
 	{
-		std::cout << "TEST with list" << std::endl;
-		std::list<int> mstack;
-		mstack.push_back(5);
-		mstack.push_back(17);
-		std::cout << mstack.back() << std::endl;
-		mstack.pop_back();
-		std::cout << mstack.size() << std::endl;
-		mstack.push_back(3);
-		mstack.push_back(5);
-		mstack.push_back(737);
-		//[...]
-		mstack.push_back(0);
-		std::list<int>::iterator it = mstack.begin();
-		std::list<int>::iterator ite = mstack.end();
+		std::cout << "TEST LIST" << std::endl;
+		std::list<int> l;
+		l.push_back(5);
+		l.push_back(17);
+		std::cout << "value on top: " << l.back() << std::endl;
+		l.pop_back();
+		std::cout << "size of the list: " << l.size() << std::endl;
+		l.push_back(3);
+		l.push_back(5);
+		l.push_back(737);
+		l.push_back(0);
+		std::cout << std::endl;
+
+		/* -- iterator -- */
+		std::cout << "iterator: ";
+		std::list<int>::iterator it = l.begin();
+		std::list<int>::iterator ite = l.end();
 		++it;
 		--it;
 		while (it != ite)
 		{
-		std::cout << *it << std::endl;
+		std::cout << *it << " ";
 		++it;
 		}
-		std::list<int> s(mstack);
+		std::cout << std::endl;
+
+		/* -- const iterator -- */
+		std::cout << "const_iterator: ";
+		std::list<int>::const_iterator	cit = l.begin();
+		std::list<int>::const_iterator	cite = l.end();
+		while (cit != cite)
+		{
+			std::cout << *cit << " ";
+			++cit;
+		}
+		std::cout << std::endl;
 	}
 	return 0;
 }
