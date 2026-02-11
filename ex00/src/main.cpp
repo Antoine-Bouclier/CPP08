5152fdaca1049f4274e776c60463b1497aa20555
Antoine <antoinebouclier1@gmail.com>
2026-02-11 10:08:42 +0100

    Ex00: test complete.

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,20 +1,79 @@
 #include "easyfind.hpp"
 #include <vector>
+#include <list>
+#include <deque>
 
 int	main(void)
 {
-	std::vector<int>	values;
+	/* -- TEST VECTOR -- */
+	{
+		std::vector<int>	values;
 
-	for (int i = 0; i < 10; i++)
-		values.push_back(i);
-	try
-	{
-		std::cout << *easyfind(values, 5) << std::endl;
-		std::cout << *easyfind(values, 20) << std::endl;
+		for (int i = 0; i < 10; i++)
+			values.push_back(i);
+		try
+		{
+			std::cout << *easyfind(values, 5) << std::endl;
+			std::cout << *easyfind(values, 20) << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+		std::vector<int>	v;
+		v.push_back(-10);
+		v.push_back(2);
+		v.push_back(0);
+		v.push_back(245201254);
+		v.push_back(2);
+		std::cout << "TEST with double occurence: " << *easyfind(v, 2) << std::endl;
+		std::cout << "TEST with negative: " << *easyfind(v, -10) << std::endl;
+		std::cout << "TEST with zero: " << *easyfind(v, 0) << std::endl;
+		std::cout << "TEST with large number: " << *easyfind(v, 245201254) << std::endl;
+
+		std::list<int> l;
+		std::cout << "TEST with no value : ";
+		try
+		{
+			easyfind(l, 10);
+		} catch (const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
-	catch(const std::exception& e)
+
+	/* -- TEST LIST -- */
 	{
-		std::cerr << e.what() << '\n';
+		std::list<int>	values;
+
+		for (int i = 0; i < 10; i++)
+			values.push_back(i);
+		try
+		{
+			std::cout << *easyfind(values, 5) << std::endl;
+			std::cout << *easyfind(values, 20) << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
-	
+
+	/* -- TEST DEQUE -- */
+	{
+		std::deque<int>	values;
+
+		for (int i = 0; i < 10; i++)
+			values.push_back(i);
+		try
+		{
+			std::cout << *easyfind(values, 5) << std::endl;
+			std::cout << *easyfind(values, 20) << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
 }
